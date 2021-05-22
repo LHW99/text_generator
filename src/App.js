b0cd5c96d4ab4a90c40fbf2728a81e8a5e6f403b
haiduk <hwlee91@gmail.com>
2021-05-22 05:30:09 -0400

    build ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import text from './data';
 
 function App() {
+  const [many, setMany] = useState(0);
+  const [displayed, setDisplayed] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(many>text.length){
+      let repeat = (Math.ceil(many/9))*9
+      setDisplayed(new Array(repeat).fill(text).flat().slice(0,many));
+      console.log(displayed.length, many)
+    } else if(many<=0) {
+      setDisplayed(text.slice(0,1))
+    } else {
+      setDisplayed(text.slice(0,many))
+    }
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section section-center">
+      <h2>Text Generator</h2>
+      <form className='lorem-form'>
+        <label for='many'>How many paragraphs?</label>
+        <input 
+          type='number' 
+          id='many'
+          name='many' 
+          value={many}
+          onChange={(e)=>setMany(e.target.value)}
+        />
+        <button className="btn" type='submit' onClick={handleSubmit}>Go!</button>
+      </form>
+      <article className="result">
+        {displayed.map((disp, index)=>{
+          return <p key={index}>{disp}</p>
+        })}
+      </article>
+    </section>
   );
 }
 
